4d0a6319a242428d8bc2e597c1ccfb42c4adfe7d
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2021-06-15 23:30:16 +0200

    Update Asin_R_code_second.r

--- Asin_R_code_second.r
+++ Asin_R_code_second.r
@@ -7,10 +7,27 @@ require(ggplot2)
 require(gridExtra)
 require(viridis)
 setwd("/Users/federicotossani/Asinara/L_image")
+source("/Users/federicotossani/Asinara/spectralrao.r")
 
 #importare file
+### Impacchettamento
 asilist<-list.files(pattern="Asi_")
-asimp<-lapply(asilist, brick)
+asimport<-lapply(asilist, raster)
+
+#Rao's Q index
+asirao<-spectralrao(asimport, distance_m="euclidean", window=3, shannon=FALSE) 
+
+
+asilist<-list.files(pattern="p193r32")
+asimp<-lapply(asilist, raster)
+asinara<-crop(asimp, extent(431858.1, 445713, 4532960, 4552802))
+asinara<-lapply(asimp, crop(asimp, extent(431858.1, 445713, 4532960, 4552802)))
+
+
+
+asirao<-spectralrao(import84, distance_m="euclidean", window=3, shannon=FALSE)
+
+
 
 asipca<-lapply(asimp, rasterPCA)
 
